e23e6defdd842b174a133e76eaf81e6e78d1e5e6
Rabindra Manandhar <rabindra.mdr@gmail.com>
2025-11-30 14:13:39 +0200

    Successfully modified scripts to deploy GKE

--- terraform/node-pools.tf
+++ terraform/node-pools.tf
@@ -81,61 +81,61 @@ resource "google_container_node_pool" "primary_nodes" {
   ]
 }
 
-# # Optional: Separate node pool for Kafka (if needed for resource isolation)
-# resource "google_container_node_pool" "kafka_nodes" {
-#   count      = 0 # Set to 1 to enable
-#   name       = "${var.cluster_name}-kafka-pool"
-#   location   = var.zone
-#   cluster    = google_container_cluster.primary.name
-#   project    = var.project_id
-#   node_count = 3
+# Optional: Separate node pool for Kafka (if needed for resource isolation)
+resource "google_container_node_pool" "kafka_nodes" {
+  count      = 1 # Set to 1 to enable
+  name       = "${var.cluster_name}-kafka-pool"
+  location   = var.zone
+  cluster    = google_container_cluster.primary.name
+  project    = var.project_id
+  node_count = 3
 
-#   autoscaling {
-#     min_node_count = 3
-#     max_node_count = 3
-#   }
+  autoscaling {
+    min_node_count = 3
+    max_node_count = 3
+  }
 
-#   management {
-#     auto_repair  = true
-#     auto_upgrade = true
-#   }
+  management {
+    auto_repair  = true
+    auto_upgrade = true
+  }
 
-#   node_config {
-#     preemptible  = false
-#     machine_type = var.node_machine_type
-#     disk_size_gb = var.disk_size_gb
-#     disk_type    = "pd-ssd" # SSD for better Kafka performance
+  node_config {
+    preemptible  = false
+    machine_type = var.node_machine_type
+    disk_size_gb = var.disk_size_gb
+    disk_type    = "pd-ssd" # SSD for better Kafka performance
 
-#     service_account = google_service_account.gke_nodes.email
-#     oauth_scopes = [
-#       "https://www.googleapis.com/auth/cloud-platform"
-#     ]
+    service_account = google_service_account.gke_nodes.email
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
 
-#     labels = {
-#       environment = "production"
-#       workload    = "kafka"
-#     }
+    labels = {
+      environment = "production"
+      workload    = "kafka"
+    }
 
-#     tags = ["gke-node", "${var.cluster_name}-kafka-node"]
+    tags = ["gke-node", "${var.cluster_name}-kafka-node"]
 
-#     taint {
-#       key    = "workload"
-#       value  = "kafka"
-#       effect = "NO_SCHEDULE"
-#     }
+    taint {
+      key    = "workload"
+      value  = "kafka"
+      effect = "NO_SCHEDULE"
+    }
 
-#     metadata = {
-#       disable-legacy-endpoints = "true"
-#     }
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
 
-#     workload_metadata_config {
-#       mode = "GKE_METADATA"
-#     }
+    workload_metadata_config {
+      mode = "GKE_METADATA"
+    }
 
-#     # Updated shielded_instance_config to satisfy org policy
-#     shielded_instance_config {
-#       enable_secure_boot          = true
-#       enable_integrity_monitoring = true
-#     }
-#   }
-# }
+    # Updated shielded_instance_config to satisfy org policy
+    shielded_instance_config {
+      enable_secure_boot          = true
+      enable_integrity_monitoring = true
+    }
+  }
+}
